1e8d5ae28ba93c18b219de9a94bf46c2e5d14054
Undavalli Naga Ravi Krishna <ravikrishna2349@gmail.com>
2026-02-25 22:58:54 +0530

    Create Reverse the elements.cpp

---
+++ STL/Vectors/Reverse the elements.cpp
@@ -0,0 +1,32 @@
+//C++ Logic to reverse the Element in a vector 
+
+#include <iostream> 
+#include <vector> 
+
+using namespace std; 
+int main() 
+{ 
+vector<int> vec={1,2,3,4,5,6}; 
+int len=vec.size(); 
+int temp=0; 
+cout<<"Before Reverse: "; 
+for(int i=0;i<len;i++) 
+{ 
+cout<<vec[i]<<" "; 
+} 
+
+for(int i=0;i<(len/2);i++) 
+{ 
+temp=vec[i]; 
+vec[i]=vec[len-i-1]; 
+vec[len-i-1]=temp; 
+} 
+
+cout<<'\n'<<"After Reverse: "; 
+for(int i=0;i<len;i++) 
+{ 
+cout<<vec[i]<<" "; 
+} 
+
+return 0; 
+}
