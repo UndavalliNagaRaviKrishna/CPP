c76b6fe776cc6d46775dbcde300366b56774f2f8
Undavalli Naga Ravi Krishna <ravikrishna2349@gmail.com>
2026-03-03 23:23:07 +0530

    Update LowestElement.cpp

--- STL/Vectors/LowestElement.cpp
+++ STL/Vectors/LowestElement.cpp
@@ -10,10 +10,10 @@
  int len=vec.size(), low=100;
  for(int i=0;i<len;i++)
  {
- if(low>vec[i])
- {
- low=vec[i];
- }
+    if(low>vec[i])
+    {
+      low=vec[i];
+    }
  }
  cout<<"Lowest Element is: "<<low;
  }
