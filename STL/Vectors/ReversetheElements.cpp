1efe7f42bb74d758585fa38bfc121a4da53487d4
Undavalli Naga Ravi Krishna <ravikrishna2349@gmail.com>
2026-03-05 23:14:04 +0530

    Update ReversetheElements.cpp

--- STL/Vectors/ReversetheElements.cpp
+++ STL/Vectors/ReversetheElements.cpp
@@ -6,27 +6,27 @@
 using namespace std; 
 int main() 
 { 
-vector<int> vec={1,2,3,4,5,6}; 
-int len=vec.size(); 
-int temp=0; 
-cout<<"Before Reverse: "; 
-for(int i=0;i<len;i++) 
-{ 
-cout<<vec[i]<<" "; 
-} 
+   vector<int> vec={1,2,3,4,5,6}; 
+   int len=vec.size(); 
+   int temp=0; 
+   cout<<"Before Reverse: "; 
+   for(int i=0;i<len;i++) 
+   { 
+      cout<<vec[i]<<" "; 
+   } 
 
-for(int i=0;i<(len/2);i++) 
-{ 
-temp=vec[i]; 
-vec[i]=vec[len-i-1]; 
-vec[len-i-1]=temp; 
-} 
+   for(int i=0;i<(len/2);i++) 
+   { 
+      temp=vec[i]; 
+      vec[i]=vec[len-i-1]; 
+      vec[len-i-1]=temp; 
+   } 
 
-cout<<'\n'<<"After Reverse: "; 
-for(int i=0;i<len;i++) 
-{ 
-cout<<vec[i]<<" "; 
-} 
+    cout<<'\n'<<"After Reverse: "; 
+    for(int i=0;i<len;i++) 
+    { 
+       cout<<vec[i]<<" "; 
+     } 
 
-return 0; 
+ return 0; 
 }
